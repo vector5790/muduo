42b666be413c79eefe1144f6d85fe5bfd3fa4ced
vector5790 <1011669365@qq.com>
2020-08-07 15:17:15 +0800

    change class name:Copyable->copyable

--- base/copyable.h
+++ base/copyable.h
@@ -4,11 +4,11 @@ A tag class emphasises the objects are copyable.
 */
 namespace muduo{
 
-class Copyable{
+class copyable{
 protected:
     //使用=default来要求编译器生成一个默认构造函数
-    Copyable() = default;
-    ~Copyable() = default;
+    copyable() = default;
+    ~copyable() = default;
 };
 
 }
