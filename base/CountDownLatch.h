9ba8fed22abde0afd46c6b67c7c6215463c292cc
vector5790 <1011669365@qq.com>
2020-08-11 08:51:35 +0800

    倒计时计数器

---
+++ base/CountDownLatch.h
@@ -0,0 +1,42 @@
+/*
+倒计时计数器
+
+1. 首先，创建一个计数器，设置 count 的初始值，程序中的数值设置为3；
+
+2. 在执行了线程 D 之后，调用 countDownLatch.wait()方法，将会进入阻塞状态，直到 countDownLatch 的 count 参数值为 0；
+
+3. 在其他线程里，调用 countDownLatch.countDown() 方法，调用该方法会将计数值 减 1；
+
+4. 当其他线程中的 countDown() 方法把计数值变成 0 时，等待线程里的 countDownLatch.wait() 立即退出，执行下面的代码。
+*/
+#ifndef MUDUO_BASE_COUNTDOWNLATCH_H
+#define MUDUO_BASE_COUNTDOWNLATCH_H
+
+#include "Condition.h"
+#include "Mutex.h"
+
+namespace muduo
+{
+class CountDownLatch  : noncopyable{
+public:
+    explicit CountDownLatch(int count);
+    //等待计数为0
+    void wait();
+    //计数减1
+    void CountDown();
+    //获得计数
+    int getCount() const;
+private:
+    mutable MutexLock mutex_;
+    /*
+    GUARDED_BY ----> THREAD_ANNOTATION_ATTRIBUTE__ ----> __attribute__
+    guarded_by属性是为了保证线程安全，使用该属性后，线程要使用相应变量，必须先锁定mutex_
+使得pendingFunctors_是原子操作。
+    */
+    Condition condition_ GUARDED_BY(mutex_);
+    int count_ GUARDED_BY(mutex_);
+};
+
+}//muduo
+
+#endif
